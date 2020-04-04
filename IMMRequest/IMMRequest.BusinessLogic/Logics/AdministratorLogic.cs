fb3462c502d985a1b74361ca36136466200010a0
invict1 <pabloinvict1@gmail.com>
2020-04-03 22:20:28 -0300

    Se creó la clase relacionada a la logica del Administrator en la capa de Business Logic y se creó el controlador para el mismo en la capa de WebApi

--- IMMRequest/IMMRequest.BusinessLogic/Logics/AdministratorLogic.cs
+++ IMMRequest/IMMRequest.BusinessLogic/Logics/AdministratorLogic.cs
@@ -1,7 +1,22 @@
-namespace IMMRequest.BusinessLogic
+using  System;
+using  System.Collections.Generic;
+using  IMMRequest.DataAccess;
+using  IMMRequest.Domain;
+
+namespace IMMRequest.BusinessLogic 
 {
-    public class AdministratorLogic
+    public class AdministratorLogic 
     {
-        
+        public AdministratorRepository administratorRepository;
+
+		public AdministratorLogic() 
+        {
+			IMMRequestContext IMMRequestContext = ContextFactory.GetNewContext();
+			this.administratorRepository = new AdministratorRepository(IMMRequestContext);
+		}
+		public Administrator Get(int id) 
+        {
+            return null;
+        }
     }
 }
--- IMMRequest/IMMRequest.WebApi/Controllers/AdministratorController.cs
+++ IMMRequest/IMMRequest.WebApi/Controllers/AdministratorController.cs
@@ -2,15 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using IMMRequest.BusinessLogic;
+using IMMRequest.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace IMMRequest.WebApi.Controllers
-{
+namespace IMMRequest.WebApi.Controllers {
     [ApiController]
     [Route("[controller]")]
-    public class AdministratorController : ControllerBase
-    {
+    public class AdministratorController : ControllerBase {
+
+        private AdministratorLogic administratorsLogic;
         
+        public  AdministratorController() {
+			administratorsLogic = new AdministratorLogic();
+		}
     }
 }
