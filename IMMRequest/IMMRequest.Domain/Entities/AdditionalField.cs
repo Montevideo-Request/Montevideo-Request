859ddeee68a50d1376001f23378a5a8e1b4c90f9
Pablo Pereira <invict1@outlook.es>
2020-04-10 20:29:35 -0300

    Se agregó el constructor con parámetros y se hizo un override al método equals a la entidad Additional Field.

--- IMMRequest/IMMRequest.Domain/Entities/AdditionalField.cs
+++ IMMRequest/IMMRequest.Domain/Entities/AdditionalField.cs
@@ -1,14 +1,15 @@
 using System.Collections.Generic;
+using System;
 
 namespace IMMRequest.Domain
 {
     public class AdditionalField
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
+
         public string Name { get; set; }
 
         public string FieldType { get; set; }
-        public Type Type { get; set; }
 
         public virtual ICollection<FieldRange> Ranges { get; set; }
 
@@ -16,5 +17,33 @@ namespace IMMRequest.Domain
         {
             this.Ranges = new List<FieldRange>();
         }
+
+        public AdditionalField(string Name, string FieldType, Type Type, ICollection<FieldRange> Ranges)
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = Name;
+            this.FieldType = FieldType;
+            this.Ranges = Ranges;
+        }
+
+        public bool IsValid() 
+        {
+            return true;
+        }
+
+        public override bool Equals(Object obj) 
+        {
+			AdditionalField additionalField = obj as AdditionalField;
+			bool equals = false;
+			if (obj == null) {
+				equals = false;
+			}
+			else {
+				equals = this.Name == additionalField.Name 
+                && this.FieldType == additionalField.FieldType;
+			}
+			return equals;
+		}
+
     }
 }
