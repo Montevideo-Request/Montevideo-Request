ec7f4fd0a123f8596a40625ca30a66f4e268285c
Pablo Pereira <invict1@outlook.es>
2020-04-10 20:33:13 -0300

    [TDD] Topic Logic Test

---
+++ IMMRequest/IMMRequest.BusinessLogic.Test/TopicTest.cs
@@ -0,0 +1,184 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using IMMRequest.Domain;
+using System.Linq;
+using System;
+
+namespace IMMRequest.BusinessLogic.Test
+{
+    [TestClass]
+    public class TopicTest 
+    {
+        public TopicLogic topicLogic;
+
+        public AdditionalFieldTest() {}
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            this.topicLogic = new TopicLogic();
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            this.topicLogic = new topicLogic();
+        }
+
+        [TestMethod]
+        public void CreateIsOk() 
+        {
+            Guid guid = Guid.NewGuid();
+	        Topic topic = new Topic() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Area = new Area()
+	        };
+            Topic result = this.topicLogic.Create(area);
+            Assert.AreEqual(topic, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateIdExists() 
+        {
+            Guid guid = Guid.NewGuid();
+            Guid anotherGuid = Guid.NewGuid();
+
+	        Topic topicExpected = new Topic() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Area = new Area()
+	        };
+            this.topicLogic.Add(topicExpected);
+            this.topicLogic.Save();
+
+            this.topicLogic.Add(topicExpected);
+            this.topicLogic.Save();
+            
+            Assert.AreEqual(topicExpected, topicExpected);
+        }
+
+        [TestMethod]
+        public void RemoveCorrectId() 
+        {
+            Guid firstGuid = Guid.NewGuid();
+            Topic firstTopicExpected = new Topic() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Area = new Area()
+	        };
+            this.topicLogic.Add(firstTopicExpected);
+            
+	        Topic secondTopicExpected = new Topic() 
+            {
+                Id = guid,
+                Name = "Just Second Testing",
+                Area = new Area()
+	        };
+            this.topicLogic.Add(secondTopicExpected);
+            this.topicLogic.Save();
+
+            this.topicLogic.Remove(firstGuid);
+
+            IEnumerable<Topic> resultList = this.topicLogic.GetTopics();
+            
+            Assert.AreEqual(1, resultList.Count());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RemoveInvalidId() 
+        {
+            Guid randomGuid = Guid.NewGuid();
+	        Topic topic = new Area() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Area = new Area()
+	        };
+            this.topicLogic.Add(topic);
+            this.topicLogic.Save();
+
+            this.topicLogic.Remove(randomGuid);
+            IEnumerable<Topic> resultList = this.topicLogic.GetTopics();
+            Assert.AreEqual(1, resultList.Count());
+        }
+
+        [TestMethod]
+        public void GetIsOk() 
+        {
+            Guid guid = Guid.NewGuid();
+
+	        Topic topicExpected = new Topic() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Area = new Area()
+	        };
+            this.topicLogic.Add(topicExpected);
+            this.topicLogic.Save();
+
+            Topic result = this.topicLogic.Get(guid);
+            
+            Assert.AreEqual(topicExpected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetIsNotOk() 
+        {
+            Guid guid = Guid.NewGuid();
+            Guid anotherGuid = Guid.NewGuid();
+
+	        Topic topicExpected = new Topic() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Area = new Area()
+	        };
+            this.topicLogic.Add(topicExpected);
+            this.topicLogic.Save();
+
+            Topic result = this.topicLogic.Get(anotherGuid);
+            
+            Assert.AreEqual(topicExpected, result);
+        }
+
+        [TestMethod]
+        public void GetTopicsIsOk() 
+        {
+	        Topic firsTopicExpected = new Topic() 
+            {
+                Id = Guid.NewGuid(),
+                Name = "Just Testing",
+                Area = new Area()
+	        };
+            this.topicLogic.Add(firstTopicExpected);
+            
+	        Topic secondTopicExpected = new Topic() 
+            {
+                Id = Guid.NewGuid(),
+                Name = "Just Testing",
+                Area = new Area()
+	        };
+            this.topicLogic.Add(secondTopicExpected);
+            this.topicLogic.Save();
+
+            IEnumerable<Topic> resultList = this.topicLogic.GetTopics();
+            
+            Assert.AreEqual(2, resultList.Count());
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetTopicsNoElements() 
+        {
+            IEnumerable<Topic> resultList = this.topicLogic.GetTopics();
+            Assert.AreEqual(0, resultList.Count());
+        }
+    }
+}
--- IMMRequest/IMMRequest.BusinessLogic/Logics/TopicLogic.cs
+++ IMMRequest/IMMRequest.BusinessLogic/Logics/TopicLogic.cs
@@ -1,7 +1,80 @@
+using  System;
+using  System.Collections.Generic;
+using  IMMRequest.DataAccess;
+using  IMMRequest.Domain;
+using System.Linq;
+
 namespace IMMRequest.BusinessLogic
 {
     public class TopicLogic
     {
-        
+        public TopicRepository topicRepository;
+
+        public TopicLogic() 
+        {
+			IMMRequestContext IMMRequestContext = ContextFactory.GetNewContext();
+			this.topicRepository = new TopicRepository(IMMRequestContext);
+		}
+
+        public void Add(Topic topic)
+        {
+            this.topicRepository.Add(topic);
+        }
+
+        public void Save()
+        {
+            this.topicRepository.Save();
+        }        
+
+        public Topic Create(Topic topic) 
+        {
+            try
+            {
+                this.Add(topic);
+                return topic;
+            } 
+            catch 
+            {
+                throw new ArgumentException("Id already exists");
+            }
+        }
+
+        public void Remove(Guid id) 
+        {
+            try 
+            {
+                Topic topic = this.topicRepository.Get(id);
+                this.topicRepository.Remove(topic);
+                this.topicRepository.Save();
+            }
+            catch
+            {
+                throw new ArgumentException("Invalid Id");
+            }
+        }
+
+		public Request Get(Guid id) 
+        {
+            try
+            {
+                return this.topicRepository.Get(id);
+            }
+            catch 
+            {
+                throw new ArgumentException("Invalid Id");
+            }
+        }
+
+        public IEnumerable<Topic> GetTopics() 
+        {
+            IEnumerable<Topic> topics = this.topicRepository.GetAll();
+            
+            if (topics.Count() == 0) 
+            {
+                throw new ArgumentException("There are no Requests");
+            }
+
+            return topics;
+		}
     }
 }
