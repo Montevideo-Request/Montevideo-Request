c624d1d5d596b55ec46b1fa82eeabec8bec6c6e0
Pablo Pereira <invict1@outlook.es>
2020-04-10 20:29:51 -0300

    Se agregó el constructor con parámetros y se hizo un override al método equals a la entidad Request.

--- IMMRequest/IMMRequest.Domain/Entities/Request.cs
+++ IMMRequest/IMMRequest.Domain/Entities/Request.cs
@@ -1,14 +1,10 @@
+using System;
+
 namespace IMMRequest.Domain
 {
-    
-/*
-La capa de Dominio es una de las mas altas, es donde van todas
-las clases que componen el dominio del negocio
-*/
-
     public class Request
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         
         public string RequestorsName { get; set; }
 
@@ -25,5 +21,44 @@ las clases que componen el dominio del negocio
         public string State { get; set; }
 
         private string Description { get; set; }
+
+        public Request(string RequestorsName, string RequestorsEmail, string RequestorsPhone, 
+                        Area Area, Topic Topic, Type Type, string State, string Description) 
+        {
+            this.Id = Guid.NewGuid();
+            this.RequestorsName = RequestorsName;
+            this.RequestorsEmail = RequestorsEmail;
+            this.RequestorsPhone = RequestorsPhone;
+            this.Area = Area;
+            this.Topic = Topic;
+            this.Type = Type;
+            this.State = State;
+            this.Description = Description;
+        }
+
+        public bool IsValid() 
+        {
+            return true;
+        }
+
+        public override bool Equals(Object obj) 
+        {
+			Request request = obj as Request;
+			bool equals = false;
+			if (obj == null) {
+				equals = false;
+			}
+			else {
+				equals = this.RequestorsName == request.RequestorsName 
+                && this.RequestorsEmail == request.RequestorsEmail
+                && this.RequestorsPhone == request.RequestorsPhone
+                && this.Area == request.Area
+                && this.Topic == request.Topic
+                && this.Type == request.Type
+                && this.State == request.State
+                && this.Description == request.Description;
+			}
+			return equals;
+		}
     }
 }
