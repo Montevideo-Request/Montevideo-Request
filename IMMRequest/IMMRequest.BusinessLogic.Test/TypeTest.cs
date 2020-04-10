28e1ac5f9c830e485be3abf55e21affeee9bbb84
Pablo Pereira <invict1@outlook.es>
2020-04-10 20:33:33 -0300

    [TDD] Type Logic Test

---
+++ IMMRequest/IMMRequest.BusinessLogic.Test/TypeTest.cs
@@ -0,0 +1,193 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using IMMRequest.Domain;
+using System.Linq;
+using System;
+
+namespace IMMRequest.BusinessLogic.Test
+{
+    [TestClass]
+    public class TypeTest 
+    {
+        public TypeLogic typeLogic;
+
+        public TypeTest() {}
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            this.typeLogic = new TypeLogic();
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            this.typeLogic = new TypeLogic();
+        }
+
+        [TestMethod]
+        public void CreateIsOk() 
+        {
+            Guid guid = Guid.NewGuid();
+	        Type type = new Type() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Topic = new Topic(),
+                AdditionalFields = new ICollection<AdditionalField>()
+	        };
+            Type result = this.typeLogic.Create(type);
+            Assert.AreEqual(type, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateIdExists() 
+        {
+            Guid guid = Guid.NewGuid();
+            Guid anotherGuid = Guid.NewGuid();
+
+	        Type typeExpected = new Type() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Topic = new Topic(),
+                AdditionalFields = new ICollection<AdditionalField>()
+	        };
+            this.typeLogic.Add(typeExpected);
+            this.typeLogic.Save();
+
+            this.typeLogic.Add(typeExpected);
+            this.typeLogic.Save();
+            
+            Assert.AreEqual(typeExpected, typeExpected);
+        }
+
+        [TestMethod]
+        public void RemoveCorrectId() 
+        {
+            Guid firstGuid = Guid.NewGuid();
+            Type firstTypeExpected = new Type() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Topic = new Topic(),
+                AdditionalFields = new ICollection<AdditionalField>()
+	        };
+            this.typeLogic.Add(firstTypeExpected);
+            
+	        Type secondTypeExpected = new Type() 
+            {
+                Id = guid,
+                Name = "Just Second Testing",
+                Topic = new Topic(),
+                AdditionalFields = new ICollection<AdditionalField>()
+	        };
+            this.typeLogic.Add(secondTypeExpected);
+            this.typeLogic.Save();
+
+            this.typeLogic.Remove(firstGuid);
+
+            IEnumerable<Type> resultList = this.typeLogic.GetTypes();
+            
+            Assert.AreEqual(1, resultList.Count());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RemoveInvalidId() 
+        {
+            Guid randomGuid = Guid.NewGuid();
+	        Type type = new Type() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Topic = new Topic(),
+                AdditionalFields = new ICollection<AdditionalField>()
+	        };
+            this.typeLogic.Add(type);
+            this.typeLogic.Save();
+
+            this.typeLogic.Remove(randomGuid);
+            IEnumerable<Type> resultList = this.typeLogic.GetTypes();
+            Assert.AreEqual(1, resultList.Count());
+        }
+
+        [TestMethod]
+        public void GetIsOk() 
+        {
+            Guid guid = Guid.NewGuid();
+
+	        Type areaExpected = new Type() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Topic = new Topic(),
+                AdditionalFields = new ICollection<AdditionalField>()
+	        };
+            this.typeLogic.Add(typeExpected);
+            this.typeLogic.Save();
+
+            Area result = this.typeLogic.Get(guid);
+            
+            Assert.AreEqual(typeExpected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetIsNotOk() 
+        {
+            Guid guid = Guid.NewGuid();
+            Guid anotherGuid = Guid.NewGuid();
+
+	        Type typeExpected = new Type() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                Topic = new Topic(),
+                AdditionalFields = new ICollection<AdditionalField>()
+	        };
+            this.typeLogic.Add(typeExpected);
+            this.typeLogic.Save();
+
+            Type result = this.typeLogic.Get(anotherGuid);
+            
+            Assert.AreEqual(typeExpected, result);
+        }
+
+        [TestMethod]
+        public void GetTypesIsOk() 
+        {
+	        Type firstTypeExpected = new Type() 
+            {
+                Id = Guid.NewGuid(),
+                Name = "Just Testing",
+                Topic = new Topic(),
+                AdditionalFields = new ICollection<AdditionalField>()
+	        };
+            this.typeLogic.Add(firstTypeExpected);
+            
+	        Type secondTypeExpected = new Type() 
+            {
+                Id = Guid.NewGuid(),
+                Name = "Just Testing",
+                Topic = new Topic(),
+                AdditionalFields = new ICollection<AdditionalField>()
+	        };
+            this.typeLogic.Add(secondTypeExpected);
+            this.typeLogic.Save();
+
+            IEnumerable<Type> resultList = this.areaLogic.GetTypes();
+            
+            Assert.AreEqual(2, resultList.Count());
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetTypesNoElements() 
+        {
+            IEnumerable<Type> resultList = this.typeLogic.GetTypes();
+            Assert.AreEqual(0, resultList.Count());
+        }
+    }
+}
--- IMMRequest/IMMRequest.BusinessLogic/Logics/TypeLogic.cs
+++ IMMRequest/IMMRequest.BusinessLogic/Logics/TypeLogic.cs
@@ -1,7 +1,80 @@
+using System;
+using System.Collections.Generic;
+using IMMRequest.DataAccess;
+using IMMRequest.Domain;
+using System.Linq;
+
 namespace IMMRequest.BusinessLogic
 {
     public class TypeLogic
     {
-        
+        public TypeRepository typeRepository;
+
+        public TypeLogic() 
+        {
+			IMMRequestContext IMMRequestContext = ContextFactory.GetNewContext();
+			this.typeRepository = new TypeRepository(IMMRequestContext);
+		}
+
+        public void Add(IMMRequest.Domain.Type type)
+        {
+            this.typeRepository.Add(type);
+        }
+
+        public void Save()
+        {
+            this.typeRepository.Save();
+        }        
+
+        public IMMRequest.Domain.Type Create(IMMRequest.Domain.Type type) 
+        {
+            try
+            {
+                this.Add(type);
+                return type;
+            } 
+            catch 
+            {
+                throw new ArgumentException("Id already exists");
+            }
+        }
+
+        public void Remove(Guid id) 
+        {
+            try 
+            {
+                IMMRequest.Domain.Type type = this.typeRepository.Get(id);
+                this.typeRepository.Remove(type);
+                this.typeRepository.Save();
+            }
+            catch
+            {
+                throw new ArgumentException("Invalid Id");
+            }
+        }
+
+		public IMMRequest.Domain.Type Get(Guid id) 
+        {
+            try
+            {
+                return this.typeRepository.Get(id);
+            }
+            catch 
+            {
+                throw new ArgumentException("Invalid Id");
+            }
+        }
+
+        public IEnumerable<IMMRequest.Domain.Type> GetTypes() 
+        {
+            IEnumerable<IMMRequest.Domain.Type> types = this.typeRepository.GetAll();
+            
+            if (types.Count() == 0) 
+            {
+                throw new ArgumentException("There are no Requests");
+            }
+
+            return types;
+		}
     }
 }
