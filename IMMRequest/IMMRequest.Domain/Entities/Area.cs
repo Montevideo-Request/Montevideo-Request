431cdac5225f4d89de08ed8394dc9f56130b93da
Pablo Pereira <invict1@outlook.es>
2020-04-10 20:29:14 -0300

    Se agregó el constructor con parámetros y se hizo un override al método equals a la entidad Area.

--- IMMRequest/IMMRequest.Domain/Entities/Area.cs
+++ IMMRequest/IMMRequest.Domain/Entities/Area.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections.Generic;
 
 namespace IMMRequest.Domain
 {
     public class Area
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         
         public string Name { get; set; }
         
@@ -14,5 +15,31 @@ namespace IMMRequest.Domain
         {
             this.Topics = new List<Topic>();
         }
+
+        public Area(string Name, string Email, ICollection<Topic> Topics) 
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = Name;
+            this.Topics = Topics;
+        }
+
+        public bool IsValid() 
+        {
+            return true;
+        }
+
+        public override bool Equals(Object obj) 
+        {
+			Area area = obj as Area;
+			bool equals = false;
+			if (obj == null) {
+				equals = false;
+			}
+			else {
+				equals = this.Name == area.Name && this.Topics == area.Topics;
+			}
+			return equals;
+		}
+
     }
 }
