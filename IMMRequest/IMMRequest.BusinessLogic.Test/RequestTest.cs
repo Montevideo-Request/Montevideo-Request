f0bb796fc2b08b6a1baac9c84dde7f882de60303
Pablo Pereira <invict1@outlook.es>
2020-04-10 20:33:50 -0300

    [TDD] Request Logic Test

---
+++ IMMRequest/IMMRequest.BusinessLogic.Test/RequestTest.cs
@@ -0,0 +1,238 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using IMMRequest.Domain;
+using System.Linq;
+using System;
+
+namespace IMMRequest.BusinessLogic.Test
+{
+    [TestClass]
+    public class RequestTest 
+    {
+        public RequestLogic requestLogic;
+
+        public RequestTest() {}
+
+        [TestInitialize()]
+        public void Initialize()
+        {
+            this.requestLogic = new RequestLogic();
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            this.requestLogic = new RequestLogic();
+        }
+
+        [TestMethod]
+        public void CreateIsOk() 
+        {
+            Guid guid = Guid.NewGuid();
+	        Request request = new Request() 
+            {
+                Id = guid,
+                RequestorsName = "Requestors Name",
+                RequestorsEmail = "Requestors Email",
+                RequestorsPhone = "RequestorsPhone",
+                Area = new Area(),
+                Topic = new Topic(),
+                Type = new Type(),
+                State = "State",
+                Description = "Description"
+	        };
+            Request result = this.requestLogic.Create(request);
+            Assert.AreEqual(request, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateIdExists() 
+        {
+            Guid guid = Guid.NewGuid();
+            Guid anotherGuid = Guid.NewGuid();
+
+	        Request requestExpected = new Request() 
+            {
+                Id = guid,
+                RequestorsName = "Just Testing",
+                RequestorsEmail = "Requestors Email",
+                RequestorsPhone = "RequestorsPhone",
+                Area = new Area(),
+                Topic = new Topic(),
+                Type = new Type(),
+                State = "State",
+                Description = "Description"
+	        };
+            this.requestLogic.Add(requestExpected);
+            this.requestLogic.Save();
+
+            this.requestLogic.Add(requestExpected);
+            this.requestLogic.Save();
+            
+            Assert.AreEqual(requestExpected, requestExpected);
+        }
+
+        [TestMethod]
+        public void RemoveCorrectId() 
+        {
+            Guid firstGuid = Guid.NewGuid();
+            Request firstRequestExpected = new Request() 
+            {
+                Id = guid,
+                RequestorsName = "Just Testing",
+                RequestorsEmail = "Requestors Email",
+                RequestorsPhone = "RequestorsPhone",
+                Area = new Area(),
+                Topic = new Topic(),
+                Type = new Type(),
+                State = "State",
+                Description = "Description"
+	        };
+            this.requestLogic.Add(firstRequestExpected);
+            
+	        Request secondRequestExpected = new Request() 
+            {
+                Id = guid,
+                RequestorsName = "Just Second Testing",
+                RequestorsEmail = "Requestors Second Email",
+                RequestorsPhone = "Requestors Second Phone",
+                Area = new Area(),
+                Topic = new Topic(),
+                Type = new Type(),
+                State = "State",
+                Description = "Description"
+	        };
+            this.requestLogic.Add(secondRequestExpected);
+            this.requestLogic.Save();
+
+            this.requestLogic.Remove(firstGuid);
+
+            IEnumerable<Request> resultList = this.requestLogic.GetRequests();
+            
+            Assert.AreEqual(1, resultList.Count());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RemoveInvalidId() 
+        {
+            Guid randomGuid = Guid.NewGuid();
+	        Request request = new Request() 
+            {
+                Id = guid,
+                RequestorsName = "Just Testing",
+                RequestorsEmail = "Requestors Email",
+                RequestorsPhone = "RequestorsPhone",
+                Area = new Area(),
+                Topic = new Topic(),
+                Type = new Type(),
+                State = "State",
+                Description = "Description"
+	        };
+            this.requestLogic.Add(request);
+            this.requestLogic.Save();
+
+            this.requestLogic.Remove(randomGuid);
+            IEnumerable<Request> resultList = this.requestLogic.GetRequests();
+            Assert.AreEqual(1, resultList.Count());
+        }
+
+        [TestMethod]
+        public void GetIsOk() 
+        {
+            Guid guid = Guid.NewGuid();
+
+	        Request requestExpected = new Request() 
+            {
+                Id = guid,
+                RequestorsName = "Just Testing",
+                RequestorsEmail = "Requestors Email",
+                RequestorsPhone = "RequestorsPhone",
+                Area = new Area(),
+                Topic = new Topic(),
+                Type = new Type(),
+                State = "State",
+                Description = "Description"
+	        };
+            this.requestLogic.Add(requestExpected);
+            this.requestLogic.Save();
+
+            Request result = this.requestLogic.Get(guid);
+            
+            Assert.AreEqual(requestExpected, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetIsNotOk() 
+        {
+            Guid guid = Guid.NewGuid();
+            Guid anotherGuid = Guid.NewGuid();
+
+	        Request requestExpected = new Request() 
+            {
+                Id = guid,
+                RequestorsName = "Just Testing",
+                RequestorsEmail = "Requestors Email",
+                RequestorsPhone = "RequestorsPhone",
+                Area = new Area(),
+                Topic = new Topic(),
+                Type = new Type(),
+                State = "State",
+                Description = "Description"
+	        };
+            this.requestLogic.Add(requestExpected);
+            this.requestLogic.Save();
+
+            Area result = this.requestLogic.Get(anotherGuid);
+            
+            Assert.AreEqual(requestExpected, result);
+        }
+
+        [TestMethod]
+        public void GetRequestsIsOk() 
+        {
+	        Request firstRequestExpected = new Request() 
+            {
+                Id = Guid.NewGuid(),
+                RequestorsName = "Just Testing",
+                RequestorsEmail = "Requestors Email",
+                RequestorsPhone = "RequestorsPhone",
+                Area = new Area(),
+                Topic = new Topic(),
+                Type = new Type(),
+                State = "State",
+                Description = "Description"
+	        };
+            this.requestLogic.Add(firstRequestExpected);
+            
+	        Request secondRequestExpected = new Request() 
+            {
+                Id = Guid.NewGuid(),
+                RequestorsName = "Just Testing",
+                RequestorsEmail = "Requestors Email",
+                RequestorsPhone = "RequestorsPhone",
+                Area = new Area(),
+                Topic = new Topic(),
+                Type = new Type(),
+                State = "State",
+                Description = "Description"
+	        };
+            this.requestLogic.Add(secondRequestExpected);
+            this.requestLogic.Save();
+
+            IEnumerable<Request> resultList = this.requestLogic.GetRequests();
+            
+            Assert.AreEqual(2, resultList.Count());
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GetRequestsNoElements() 
+        {
+            IEnumerable<Request> resultList = this.requestLogic.GetRequests();
+            Assert.AreEqual(0, resultList.Count());
+        }
+    }
+}
--- IMMRequest/IMMRequest.BusinessLogic/Logics/RequestLogic.cs
+++ IMMRequest/IMMRequest.BusinessLogic/Logics/RequestLogic.cs
@@ -1,30 +1,80 @@
+using System;
 using System.Collections.Generic;
+using IMMRequest.DataAccess;
 using IMMRequest.Domain;
+using System.Linq;
 
 namespace IMMRequest.BusinessLogic
 {
-    
-/* 
-Business Logic va a ser la capa que es encargada de hacer
-las funcionalidades con los objetos del dominio para
-cumplir los requerimientos, Aca va a estar toda la logica.
-
-Va a usar el acceso a datos para poder persistir los datos
-(borrar, guardar, obtenerlos...) 
-*/
-
     public class RequestLogic
     {
-        public List<Request> GetAll()
+        public RequestRepository requestRepository;
+
+        public RequestLogic() 
         {
-            return new List<Request>()
-            {
-                new Request()
-                {
-                    Id = 0,
-                    RequestorsName = "Test Name"
-                }
-            };
+			IMMRequestContext IMMRequestContext = ContextFactory.GetNewContext();
+			this.requestRepository = new RequestRepository(IMMRequestContext);
+		}
+
+        public void Add(Request request)
+        {
+            this.requestRepository.Add(request);
         }
+
+        public void Save()
+        {
+            this.requestRepository.Save();
+        }        
+
+        public Request Create(Request request) 
+        {
+            try
+            {
+                this.Add(request);
+                return request;
+            } 
+            catch 
+            {
+                throw new ArgumentException("Id already exists");
+            }
+        }
+
+        public void Remove(Guid id) 
+        {
+            try 
+            {
+                Request request = this.requestRepository.Get(id);
+                this.requestRepository.Remove(request);
+                this.requestRepository.Save();
+            }
+            catch
+            {
+                throw new ArgumentException("Invalid Id");
+            }
+        }
+
+		public Request Get(Guid id) 
+        {
+            try
+            {
+                return this.requestRepository.Get(id);
+            }
+            catch 
+            {
+                throw new ArgumentException("Invalid Id");
+            }
+        }
+
+        public IEnumerable<Request> GetAll() 
+        {
+            IEnumerable<Request> requests = this.requestRepository.GetAll();
+            
+            if (requests.Count() == 0) 
+            {
+                throw new ArgumentException("There are no Requests");
+            }
+
+            return requests;
+		}
     }
 }
