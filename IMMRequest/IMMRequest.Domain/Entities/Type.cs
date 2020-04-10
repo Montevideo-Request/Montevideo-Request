dcb311aea78f1c0edbc6d0ceb5140b413f5965a1
Pablo Pereira <invict1@outlook.es>
2020-04-10 20:30:17 -0300

    Se agregó el constructor con parámetros y se hizo un override al método equals a la entidad Type.

--- IMMRequest/IMMRequest.Domain/Entities/Type.cs
+++ IMMRequest/IMMRequest.Domain/Entities/Type.cs
@@ -1,10 +1,12 @@
 using System.Collections.Generic;
+using System;
 
 namespace IMMRequest.Domain
 {
     public class Type
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
+
         public string Name { get; set; }
 
         public Topic Topic { get; set; }
@@ -15,5 +17,33 @@ namespace IMMRequest.Domain
         {
             this.AdditionalFields = new List<AdditionalField>();
         }
+
+        public Type(string Name, Topic Topic, ICollection<AdditionalField> AdditionalFields) 
+        {
+            this.Id = Guid.NewGuid();
+            this.Name = Name;
+            this.Topic = Topic;
+            this.AdditionalFields = AdditionalFields;
+        }
+
+        public bool IsValid() 
+        {
+            return true;
+        }
+
+        public override bool Equals(Object obj) 
+        {
+			Type type = obj as Type;
+			bool equals = false;
+			if (obj == null) {
+				equals = false;
+			}
+			else {
+				equals = this.Name == type.Name 
+                && this.Topic == type.Topic
+                && this.AdditionalFields == type.AdditionalFields;
+			}
+			return equals;
+		}
     }
 }
