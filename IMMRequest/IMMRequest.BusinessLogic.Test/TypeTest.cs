19ab29ade356ceeb964d84fbd343ab738f75c909
Pablo Pereira <invict1@outlook.es>
2020-04-20 15:35:11 -0300

    Se implementó la herencia de TypeLogic a BaseLogic y se añadieron las pruebas con Mock

--- IMMRequest/IMMRequest.BusinessLogic.Test/TypeTest.cs
+++ IMMRequest/IMMRequest.BusinessLogic.Test/TypeTest.cs
@@ -1,9 +1,9 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
-using IMMRequest.Domain;
-using System.Linq;
 using System;
 using IMMRequest.DataAccess.Interface;
+using IMMRequest.Domain;
+using Moq;
+using System.Collections.Generic;
 
 namespace IMMRequest.BusinessLogic.Test
 {
@@ -35,6 +35,113 @@ namespace IMMRequest.BusinessLogic.Test
         public override Type ModifyEntity(Type Entity)
         {
             throw new NotImplementedException();
+        }
+
+        [TestMethod]
+        public void CreateCaseNotExist() 
+        {
+            Guid guid = Guid.NewGuid();
+	        TypeEntity type = new TypeEntity() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                TopicId = Guid.NewGuid()
+	        };
+
+            var mock = new Mock<IRepository<TypeEntity>>(MockBehavior.Strict);
+            mock.Setup(m => m.Add(It.IsAny<TypeEntity>()));
+            mock.Setup(m => m.Save());
+
+            var controller = new TypeLogic(mock.Object);
+            Guid result = controller.Create(type);
+
+            mock.VerifyAll();
+            Assert.AreEqual(result, guid);
+        }
+
+        
+        [TestMethod]
+        public void CreateInvalidId() 
+        {
+            TypeEntity type = new TypeEntity();
+            var mock = new Mock<IRepository<TypeEntity>>(MockBehavior.Strict);
+            mock.Setup(m => m.Add(type)).Throws(new ArgumentException());
+
+            var controller = new TypeLogic(mock.Object);
+            Assert.ThrowsException<ArgumentException>(() => controller.Create(type));
+            mock.VerifyAll();
+        }
+
+        [TestMethod]
+        public void GetIsOk() 
+        {
+            Guid guid = Guid.NewGuid();
+	        TypeEntity type = new TypeEntity() 
+            {
+                Id = guid,
+                Name = "Just Testing",
+                TopicId = Guid.NewGuid()
+	        };
+            
+            var mock = new Mock<IRepository<TypeEntity>>(MockBehavior.Strict);
+            mock.Setup(m => m.Get(guid)).Returns(type);
+            var controller = new TypeLogic(mock.Object);
+            
+            TypeEntity result = controller.Get(guid);
+            Assert.AreEqual(type, result);
+        }
+
+        [TestMethod]
+        public void GetIsNotOk() 
+        {
+            Guid guid = Guid.NewGuid();
+            var mock = new Mock<IRepository<TypeEntity>>(MockBehavior.Strict);
+            mock.Setup(m => m.Get(guid)).Throws(new ArgumentException());
+            var controller = new TypeLogic(mock.Object);
+
+            Assert.ThrowsException<ArgumentException>(() => controller.Get(guid));
+            mock.VerifyAll();
+        }
+
+        [TestMethod]
+        public void GetAllIsOk() 
+        {
+	        TypeEntity firstTypeExpected = new TypeEntity() 
+            {
+                Id = Guid.NewGuid(),
+                Name = "Just Testing",
+                TopicId = Guid.NewGuid()
+	        };
+                
+            TypeEntity secondTypeExpected = new TypeEntity() 
+            {
+                Id = Guid.NewGuid(),
+                Name = "Second Just Testing",
+                TopicId = Guid.NewGuid()
+	        };
+
+            IEnumerable<TypeEntity> types = new List<TypeEntity>(){ 
+                firstTypeExpected, 
+                secondTypeExpected 
+            };
+
+            var mock = new Mock<IRepository<TypeEntity>>(MockBehavior.Strict);
+            mock.Setup(m => m.GetAll()).Returns(types);
+            var controller = new TypeLogic(mock.Object);
+            
+            IEnumerable<TypeEntity> resultList = controller.GetAll();
+            Assert.AreEqual(types, resultList);
+        }
+        
+        [TestMethod]
+        public void GetAllNoElements() 
+        {
+            var mock = new Mock<IRepository<TypeEntity>>(MockBehavior.Strict);
+            mock.Setup(m => m.GetAll()).Throws(new ArgumentException());
+            var controller = new TypeLogic(mock.Object);
+
+            Assert.ThrowsException<ArgumentException>(() => controller.GetAll());
+            mock.VerifyAll();
         }
     }
 }
