9b874d8a6d8cfd9d04bd6c07f02548244934e42b
Pablo Pereira <invict1@outlook.es>
2020-04-23 09:29:55 -0300

    Se creó el controlador para las expcetions.

---
+++ IMMRequest/IMMRequest.Exceptions/ExceptionController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMMRequest.BusinessLogic
+{
+   public class ExceptionController: Exception
+    {
+        public ExceptionController(): base(){ }
+
+        public ExceptionController(string message) : base(message) { }
+
+        public ExceptionController(string message, Exception innerException)
+            : base(message, innerException){ }
+    }
+}
