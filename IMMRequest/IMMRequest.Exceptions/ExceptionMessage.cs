54fd8bf1cdba09d34a19598ffa3b540a97bcb79d
Pablo Pereira <invict1@outlook.es>
2020-04-23 09:30:36 -0300

    Se crearon los primeros mensajes genéricos para las exceptions.

---
+++ IMMRequest/IMMRequest.Exceptions/ExceptionMessage.cs
@@ -0,0 +1,10 @@
+namespace IMMRequest.Exceptions
+{
+    public class ExceptionMessage
+    {
+        public const string NOT_IMPLEMENTED = "Method not implemented.";
+        public const string INVALID_ID = "Invalid id.";
+        public const string INVALID_CREDENTIALS = "Invalid credentials.";
+        public const string NO_ELEMENTS_FOUND = "No items found.";
+    }
+}
