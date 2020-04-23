7eb5b1cfed1bea35d0e7197f429dbb01f697a23b
Pablo Pereira <invict1@outlook.es>
2020-04-23 09:32:36 -0300

    Se cambiaron los argument exceptions por nuestras customs exceptions en la clase de logica Type.

--- IMMRequest/IMMRequest.BusinessLogic/Logics/TypeLogic.cs
+++ IMMRequest/IMMRequest.BusinessLogic/Logics/TypeLogic.cs
@@ -1,7 +1,7 @@
 using IMMRequest.DataAccess.Interface;
 using IMMRequest.DataAccess;
 using IMMRequest.Domain;
-using System;
+using IMMRequest.Exceptions;
 
 
 namespace IMMRequest.BusinessLogic
@@ -30,7 +30,7 @@ namespace IMMRequest.BusinessLogic
             }
             catch
             {
-                throw new ArgumentException("Invalid guid");
+                throw new ExceptionController(ExceptionMessage.NOT_IMPLEMENTED);
             } 
         }
     }
