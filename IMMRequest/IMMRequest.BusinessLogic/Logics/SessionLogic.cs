a38cab9662bb6aa60a41738632701ecb532eea52
Pablo Pereira <invict1@outlook.es>
2020-04-22 22:02:03 -0300

    Se creó la clase SessionLogic y se implementaron los métodos de Login y validacion de Token.

---
+++ IMMRequest/IMMRequest.BusinessLogic/Logics/SessionLogic.cs
@@ -0,0 +1,44 @@
+using System;
+using IMMRequest.BusinessLogic.Interface;
+using IMMRequest.DataAccess;
+using IMMRequest.Domain;
+
+namespace Moodle.BusinessLogic
+{
+    public class SessionLogic : ISessionLogic
+    {
+        private readonly AdministratorRepository administratorRepository;
+        
+        public SessionLogic(AdministratorRepository _administratorRepository)
+        {
+            this.administratorRepository = _administratorRepository;
+        }
+
+        public Guid Login(string email, string password)
+        {
+            try
+            {
+                var administrator = this.administratorRepository.Get(s => s.Email == email && s.Password == password);
+
+                if(administrator.Token == Guid.Empty)
+                {
+                    administrator.Token = Guid.NewGuid();
+
+                    this.administratorRepository.Update(administrator);
+                    this.administratorRepository.Save();
+                }
+
+                return administrator.Token;
+            }
+            catch(Exception)
+            {
+                throw new Exception("Invalid credentials");
+            }
+        }
+
+        public bool IsValidToken(Guid token)
+        {
+            return this.administratorRepository.Exist(s => s.Token == token);
+        }
+    }
+}
