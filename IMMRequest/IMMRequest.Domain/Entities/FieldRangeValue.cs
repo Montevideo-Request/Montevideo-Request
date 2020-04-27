7f2e651490731d6aebeb54c380d5b81a11742b77
jtouris <jtouris@netsuite.com>
2020-04-27 00:44:18 -0300

    Se creo una nueva entidad para mapear la relacion entre los campos adicionales y la request

---
+++ IMMRequest/IMMRequest.Domain/Entities/FieldRangeValue.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace IMMRequest.Domain
+{
+    public class FieldRangeValue
+    {
+        public Guid Id { get; set; }
+        public Guid RequestId { get; set; }
+        public Request Request { get; set; }
+        public AdditionalField AdditionalField { get; set;}
+        public Guid AdditionalFieldId { get; set;}
+        public string Value { get; set; }
+
+        public FieldRangeValue () { }
+    }
+}
