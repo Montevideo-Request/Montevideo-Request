7dc38996a95f6431289f51fa2783c0f8d3c1c08c
Pablo Pereira <invict1@outlook.es>
2020-04-30 10:28:23 -0300

    Se añadieron las validaciones relacionadas al Topic

--- IMMRequest/IMMRequest.BusinessLogic/Logics/TopicLogic.cs
+++ IMMRequest/IMMRequest.BusinessLogic/Logics/TopicLogic.cs
@@ -2,6 +2,8 @@ using IMMRequest.DataAccess.Interface;
 using IMMRequest.DataAccess;
 using IMMRequest.Domain;
 using IMMRequest.BusinessLogic.Interface;
+using System;
+using System.Collections.Generic;
 
 namespace IMMRequest.BusinessLogic
 {
@@ -25,7 +27,44 @@ namespace IMMRequest.BusinessLogic
 
         public override void IsValid(Topic topic)
         { 
-            return ;
+            if(topic.Name.Length == 0)
+            {
+                throw new ExceptionController(ExceptionMessage.INVALID_LENGTH);
+            }
+            if(ContainsTopic(topic.Name, topic.AreaId))
+            {
+                throw new ExceptionController(ExceptionMessage.TOPIC_ALREADY_EXISTS);
+            }
+            NotExist(topic.Name);
+        }
+
+        public bool ContainsTopic(string name, Guid areaId)
+        {
+            bool containsType = false;
+            Area area = this.repository.GetParent(areaId);
+            Topic dummyTopic = new Topic();
+            dummyTopic.Name = name;
+            if(!area.Topics.Contains(dummyTopic))
+            {
+                containsType = true;
+            }
+            return containsType;
+        }
+
+        private void NotExist(string name)
+        {
+            if (repository.Exist(a => a.Name == name))
+            {
+                throw new ExceptionController(ExceptionMessage.TOPIC_ALREADY_EXISTS);
+            }
+        }
+
+        public override void EntityExists(Guid id)
+        {
+            if (!repository.Exist(a => a.Id == id))
+            {
+                throw new ExceptionController(ExceptionMessage.INVALID_TOPIC_ID);
+            }
         }
     }
 }
