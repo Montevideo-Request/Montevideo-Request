1d6eb515b589fbc7a8641061587b9ab047ca35aa
Pablo Pereira <invict1@outlook.es>
2020-04-30 10:13:42 -0300

    Se cambió la ubicación de los archivos de exceptions

--- IMMRequest/IMMRequest.BusinessLogic.Interface/ExceptionMessage.cs
+++ IMMRequest/IMMRequest.BusinessLogic.Interface/ExceptionMessage.cs
@@ -8,5 +8,23 @@ namespace IMMRequest.BusinessLogic.Interface
         public const string NO_ELEMENTS_FOUND = "No items found.";
         public const string EMAIL_IN_USE = "This email is already in use.";
         public const string INVALID_EMAIL_FORMAT = "Invalid email format.";
+        public const string INVALID_AREA_ID = "Invalid area id.";
+        public const string INVALID_TOPIC_ID = "Invalid topic id.";
+        public const string INVALID_TYPE_ID = "Invalid type id.";
+        public const string INVALID_ADMINISTRATOR_ID = "Invalid administrator id.";
+        public const string INVALID_ADDITIONAL_FIELD_ID = "Invalid additional field id.";
+        public const string INVALID_PHONE_FORMAT = "Invalid phone format.";
+        public const string INVALID_ADDITIONAL_FIELD = "Invalid additional field.";
+        public const string INVALID_LENGTH = "Invalid input size.";
+        public const string WRONG_AREA_ID = "The given area id does not match with the original area.";
+        public const string WRONG_TYPE_ID = "The given type id does not match with the original type.";
+        public const string WRONG_TOPIC_ID = "The given topic id does not match with the original topic.";
+        public const string WRONG_DATE_FORMAT = "The given date format is invalid, must be 'MM/dd/yyyy'.";
+        public const string WRONG_INTEGER_FORMAT = "The given integer is invalid.";
+        public const string ADDITIONAL_FIELD_ALREADY_EXISTS = "This additional field already exists.";
+        public const string TYPE_ALREADY_EXISTS = "This type already exists.";
+        public const string AREA_ALREADY_EXISTS = "This area already exists.";
+        public const string TOPIC_ALREADY_EXISTS = "This topic already exists.";
+        public static string RANGE_NOT_LISTED = "This range is not listed.";
     }
 }
--- IMMRequest/IMMRequest.BusinessLogic.Interface/ILogic.cs
+++ IMMRequest/IMMRequest.BusinessLogic.Interface/ILogic.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using IMMRequest.Domain;
 
 namespace IMMRequest.BusinessLogic.Interface
 {
