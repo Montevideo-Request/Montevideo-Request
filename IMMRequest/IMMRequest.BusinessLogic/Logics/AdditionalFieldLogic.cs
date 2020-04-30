2b1fa80f1a61db03e0288e5e0d0ff24e1778a5c4
Pablo Pereira <invict1@outlook.es>
2020-04-30 10:25:50 -0300

    Se añadieron las validaciones relacionadas al AdditionalField

--- IMMRequest/IMMRequest.BusinessLogic/Logics/AdditionalFieldLogic.cs
+++ IMMRequest/IMMRequest.BusinessLogic/Logics/AdditionalFieldLogic.cs
@@ -2,6 +2,8 @@ using IMMRequest.DataAccess.Interface;
 using IMMRequest.DataAccess;
 using IMMRequest.BusinessLogic.Interface;
 using IMMRequest.Domain;
+using System;
+using System.Collections.Generic;
 
 namespace IMMRequest.BusinessLogic
 {
@@ -34,9 +36,54 @@ namespace IMMRequest.BusinessLogic
             }   
         }
 
-        public override void IsValid(AdditionalField additional)
+        public override void IsValid(AdditionalField additionalField)
         { 
-            return ;
+            if(additionalField.Name.Length == 0)
+            {
+                throw new ExceptionController(ExceptionMessage.INVALID_LENGTH);
+            }
+            if(additionalField.FieldType.Length == 0)
+            {
+                throw new ExceptionController(ExceptionMessage.INVALID_LENGTH);
+            }
+            if(ContainsAdditionalField(additionalField.Name, additionalField.TypeId))
+            {
+                throw new ExceptionController(ExceptionMessage.ADDITIONAL_FIELD_ALREADY_EXISTS);
+            }
+        }
+
+        public bool ContainsAdditionalField(string name, Guid typeId)
+        {
+            bool containsAdditionalFiled = false;
+            TypeEntity type = this.repository.GetParent(typeId);
+            AdditionalField dummyAdditionalField = new AdditionalField();
+            dummyAdditionalField.Name = name;
+            if(!type.AdditionalFields.Contains(dummyAdditionalField))
+            {
+                containsAdditionalFiled = true;
+            }
+            return containsAdditionalFiled;
+        }
+
+        
+        public override void EntityExists(Guid id)
+        {
+            if (!repository.Exist(a => a.Id == id))
+            {
+                throw new ExceptionController(ExceptionMessage.INVALID_ADDITIONAL_FIELD_ID);
+            }
+        }
+
+        internal void ContainsRange(Guid additionalFieldId, ICollection<FieldRange> ranges, IRepository<AdditionalField> additionalFieldRepository)
+        {
+            AdditionalField additionalField = this.repository.Get(additionalFieldId);
+            foreach(FieldRange range in ranges)
+            {
+                if(!additionalField.Ranges.Contains(range))
+                {
+                    throw new ExceptionController(ExceptionMessage.RANGE_NOT_LISTED);
+                }
+            }
         }
     }
 }
