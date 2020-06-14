89dd1308e5d4ee9e9f3735206666185dbc4ff3e6
Pablo Pereira <invict1@outlook.es>
2020-06-14 01:08:59 -0300

    Se añadieron los environments y contenido inicial relacionado a la app.

--- Frontend/IMMRequest/src/app/app-routing.module.ts
+++ Frontend/IMMRequest/src/app/app-routing.module.ts
@@ -1,8 +1,48 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './components/home/home.component';
+import { PanelComponent } from './components/panel/panel.component';
 
-const routes: Routes = [];
+import { ManageAreasComponent } from './components/admin/areas/manage-areas/manage-areas.component';
+import { ManageTopicsComponent } from './components/admin/topics/manage-topics/manage-topics.component';
+import { ManageTypesComponent } from './components/admin/types/manage-types/manage-types.component';
+import { ManageAdditionalFieldsComponent } from './components/admin/additional-fields/manage-additional-fields//manage-additional-fields.component';
+import { ManageAdministratorsComponent } from './components/admin/administrators/manage-administrators/manage-administrators.component';
+
+import { AdditionalFieldsComponent } from './components/additional-fields/additional-fields.component';
+import { TypesComponent } from './components/types/types.component';
+import { TopicsComponent } from './components/topics/topics.component';
+import { AreasComponent } from './components/areas/areas.component';
+import { RequestComponent } from './components/request/request.component';
+import { ReportsComponent } from './components/admin/reports/reports.component';
+
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+
+import { AdminGuard } from './guards/admin.guard';
+import { AuthGuard } from './guards/auth.guard';
+import { LoginGuard } from './guards/login.guard';
+
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'request/:id', component: RequestComponent, canActivate: [AuthGuard]},
+  {path: 'admin/administrators', component: ManageAdministratorsComponent, canActivate: [AdminGuard]},
+  {path: 'admin/areas', component: ManageAreasComponent, canActivate: [AdminGuard]},
+  {path: 'admin/topics', component: ManageTopicsComponent, canActivate: [AdminGuard]},
+  {path: 'admin/types', component: ManageTypesComponent, canActivate: [AdminGuard]},
+  {path: 'admin/additionalFields', component: ManageAdditionalFieldsComponent, canActivate: [AdminGuard]},
+  {path: 'admin/reports', component: ReportsComponent, canActivate: [AdminGuard]},
+  {path: 'admin/panel', component: PanelComponent, canActivate: [AdminGuard]},
+  {path: 'areas', component: AreasComponent, canActivate: [AuthGuard]},
+  {path: 'topics', component: TopicsComponent, canActivate: [AuthGuard]},
+  {path: 'types', component: TypesComponent, canActivate: [AuthGuard]},
+  {path: 'additionalFields', component: AdditionalFieldsComponent, canActivate: [AuthGuard]},
+  {path: 'login', component: LoginComponent, canActivate: [LoginGuard]},
+  {path: 'logout', component: LogoutComponent},
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: '**', redirectTo: '/home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/IMMRequest/src/app/app.module.ts
+++ Frontend/IMMRequest/src/app/app.module.ts
@@ -8,6 +8,30 @@ import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ReportsComponent } from './components/admin/reports/reports.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { ManageFollowersComponent } from './components/admin/administrators/manage-followers/manage-followers.component';
+import { ManageAdministratorsComponent } from './components/admin/administrators/manage-administrators/manage-administrators.component';
+import { RequestComponent } from './components/request/request.component';
+import { AreasComponent } from './components/areas/areas.component';
+import { TypesComponent } from './components/types/types.component';
+import { TopicsComponent } from './components/topics/topics.component';
+import { AdditionalFieldsComponent } from './components/additional-fields/additional-fields.component';
+import { PanelComponent } from './components/panel/panel.component';
+import { ManageAreasComponent } from './components/admin/areas/manage-areas/manage-areas.component';
+import { ManageTopicsComponent } from './components/admin/topics/manage-topics/manage-topics.component';
+import { ManageTypesComponent } from './components/admin/types/manage-types/manage-types.component';
+import { ManageAdditionalFieldsComponent } from './components/admin/additional-fields/manage-additional-fields/manage-additional-fields.component';
+import { AdministratorListFilterPipe } from './pipes/administrator-list-filter.pipe';
+import { AreasListFilterPipe } from './pipes/areas-list-filter.pipe';
+import { TopicsListFilterPipe } from './pipes/topics-list-filter.pipe';
+import { TypesListFilterPipe } from './pipes/types-list-filter.pipe';
+import { AdditionalFieldsListFilterPipe } from './pipes/additional-fields-list-filter.pipe';
 
 @NgModule({
   declarations: [
@@ -16,9 +40,33 @@ import { ReportsComponent } from './components/admin/reports/reports.component';
     LoginComponent,
     LogoutComponent,
     FooterComponent,
-    ReportsComponent
+    ReportsComponent,
+    NavbarComponent,
+    WelcomeComponent,
+    ManageFollowersComponent,
+    ManageAdministratorsComponent,
+    RequestComponent,
+    AreasComponent,
+    TypesComponent,
+    TopicsComponent,
+    AdditionalFieldsComponent,
+    PanelComponent,
+    ManageAreasComponent,
+    ManageTopicsComponent,
+    ManageTypesComponent,
+    ManageAdditionalFieldsComponent,
+    AdministratorListFilterPipe,
+    AreasListFilterPipe,
+    TopicsListFilterPipe,
+    TypesListFilterPipe,
+    AdditionalFieldsListFilterPipe
   ],
   imports: [
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatCardModule,
+    MatToolbarModule,
     BrowserModule,
     AppRoutingModule
   ],
