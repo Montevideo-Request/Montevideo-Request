0ce543a2fd74b98b9629c2df491c128cc71f6b3f
Pablo Pereira <invict1@outlook.es>
2020-06-16 14:49:46 -0300

    Se implementó el componente manage-administrators.

--- Frontend/IMMRequest/src/app/components/admin/administrators/manage-administrators/manage-administrators.component.ts
+++ Frontend/IMMRequest/src/app/components/admin/administrators/manage-administrators/manage-administrators.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Administrator } from './../../../../models/administrator';
+import { AdministratorBasicInfo } from './../../../../models/administratorBasicInfo';
+import { AddAdministratorComponent } from '../add-administrator/add-administrator.component';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { faUserPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faUserEdit } from '@fortawesome/free-solid-svg-icons';
+import { faUserSlash } from '@fortawesome/free-solid-svg-icons';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { EditAdministratorComponent } from '../edit-administrator/edit-administrator.component';
+import { Subscription } from 'rxjs';
+import { AdministratorService } from '../../../../services/administrator.service';
 
 @Component({
   selector: 'app-manage-administrators',
@@ -6,10 +16,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-administrators.component.css']
 })
 export class ManageAdministratorsComponent implements OnInit {
+  response: any = { keys: "", body: "" };
+  faUserPlus = faUserPlus;
+  faUserEdit = faUserEdit;
+  faUserRemove = faUserSlash;
+  faSearch = faSearch;
+  administrators: AdministratorBasicInfo[];
+  administrator: Administrator;
+  subscriptions: Subscription[] = [];
+  bsModalRef: BsModalRef;
+  listFilter: '';
+  constructor(
+    private modalService: BsModalService,
+    private changeDetection: ChangeDetectorRef,
+    private administratorService: AdministratorService
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.modalService.onHide.subscribe(() => {
+      this.administratorService
+        .GetAll()
+        .subscribe((users: AdministratorBasicInfo[]) => this.administrators = users, messageError => this.response.body = messageError);
+    });
+    this.administratorService
+      .GetAll()
+      .subscribe((users: AdministratorBasicInfo[]) => this.administrators = users, messageError => this.response.body = messageError);
 
-  ngOnInit(): void {
   }
 
+  edit(item: Administrator) {
+    this.openEditModal(item);
+  }
+
+  delete(administrator: AdministratorBasicInfo): void {
+    this.administrators = this.administrators.filter(h => h !== administrator);
+    this.administratorService.delete(administrator).subscribe();
+  }
+
+  add(): void {
+    this.openRegisterModal();
+  }
+
+  openRegisterModal() {
+    this.bsModalRef = this.modalService.show(AddAdministratorComponent, {
+      class: 'modal-lg'
+    });
+    this.bsModalRef.content.closeBtnName = 'Close';
+  }
+
+  openEditModal(item: Administrator) {
+    const initialState = {
+      administrator: item
+    };
+    this.bsModalRef = this.modalService.show(EditAdministratorComponent, {
+      class: 'modal-lg',
+      initialState
+    });
+    this.bsModalRef.content.closeBtnName = 'Close';
+  }
 }
