8ef5c8bbcaab83ccee61ffe1c7e25c607aa00202
Pablo Pereira <invict1@outlook.es>
2020-06-19 14:52:52 -0300

    Se añadió un custom pipe relacionado a las requests.

---
+++ Frontend/IMMRequest/src/app/pipes/requests-list-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RequestsListFilterPipe } from './requests-list-filter.pipe';
+
+describe('RequestsListFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new RequestsListFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ Frontend/IMMRequest/src/app/pipes/requests-list-filter.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Request } from '../models/request';
+
+@Pipe({
+  name: 'requestsListFilter'
+})
+export class RequestsListFilterPipe implements PipeTransform {
+
+  transform(value: Request[], filterBy: string): Request[] {
+    filterBy = filterBy ? filterBy.toLocaleLowerCase() : null;
+
+    return filterBy ? value.filter((request: Request) =>
+        request.RequestorsEmail.toLocaleLowerCase().indexOf(filterBy) !== -1) : value;
+  }
+}
