fc86f1d6f27e596340496a23bd37b8c8d831281c
Pablo Pereira <invict1@outlook.es>
2020-06-19 14:50:20 -0300

    Se implementó el servició Parser.

---
+++ Frontend/IMMRequest/src/app/services/parser.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { environment } from './../../environments/environment';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Parser } from '../models/parser';
+import { Format } from '../models/format';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParserService {
+  constructor(private http: HttpClient) { }
+  reqHeader = new HttpHeaders({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': 'true',
+    'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token',
+    // Authorization: `Bearer ${localStorage.getItem('access_token')}`,
+    'Content-Type': 'application/json; charset=UTF-8'
+  });
+
+  GetFormats(): Observable<Format[]> {
+    return this.http
+      .get<Format[]>(
+        `${environment.apiUrl}/parsers`)
+      .pipe(
+        catchError(this.errorHandler
+        )
+      );
+  }
+
+  Convert(parser: Parser): Observable<Parser> {
+    return this.http
+      .post<Parser>(
+        `${environment.apiUrl}/parsers`,
+        parser,
+        { headers: this.reqHeader })
+      .pipe(catchError(this.errorHandler));
+  }
+
+  private errorHandler(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      if (error.status === 400 || error.status === 403) {
+        return throwError(error.error.Error);
+      }
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error.Error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
+
+}
