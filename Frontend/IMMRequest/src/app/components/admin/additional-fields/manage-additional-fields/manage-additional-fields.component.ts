3640b7d0137069c2726144b914252a276ee250b7
Pablo Pereira <invict1@outlook.es>
2020-06-20 02:28:06 -0300

    Se implementó el componente manage-additional-fields relacionado al admin.

--- Frontend/IMMRequest/src/app/components/admin/additional-fields/manage-additional-fields/manage-additional-fields.component.ts
+++ Frontend/IMMRequest/src/app/components/admin/additional-fields/manage-additional-fields/manage-additional-fields.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { AdditionalField } from './../../../../models/additionalField';
+import { AddAdditionalFieldComponent } from '../add-additional-field/add-additional-field.component';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { faUserPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faUserEdit } from '@fortawesome/free-solid-svg-icons';
+import { faUserSlash } from '@fortawesome/free-solid-svg-icons';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { EditAdditionalFieldComponent } from '../edit-additional-field/edit-additional-field.component';
+import { Subscription } from 'rxjs';
+import { AdditionalFieldService } from '../../../../services/additional-field.service';
 
 @Component({
   selector: 'app-manage-additional-fields',
@@ -6,10 +15,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-additional-fields.component.css']
 })
 export class ManageAdditionalFieldsComponent implements OnInit {
+  response: any = { keys: "", body: "" };
+  faUserPlus = faUserPlus;
+  faUserEdit = faUserEdit;
+  faUserRemove = faUserSlash;
+  faSearch = faSearch;
+  additionalFields: AdditionalField[];
+  additionalField: AdditionalField;
+  subscriptions: Subscription[] = [];
+  bsModalRef: BsModalRef;
+  listFilter: '';
+  constructor(
+    private modalService: BsModalService,
+    private changeDetection: ChangeDetectorRef,
+    private additionalFieldService: AdditionalFieldService
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.modalService.onHide.subscribe(() => {
+      this.additionalFieldService
+        .getAdditionalFields()
+        .subscribe((additionalFields: AdditionalField[]) =>
+          this.additionalFields = additionalFields, messageError => this.response.body = messageError);
+    });
+    this.additionalFieldService
+      .getAdditionalFields()
+      .subscribe((additionalFields: AdditionalField[]) =>
+        this.additionalFields = additionalFields, messageError => this.response.body = messageError);
 
-  ngOnInit(): void {
   }
 
+  edit(item: AdditionalField) {
+    this.openEditModal(item);
+  }
+
+  delete(additionalField: AdditionalField): void {
+    this.additionalFields = this.additionalFields.filter(h => h !== additionalField);
+    this.additionalFieldService.delete(additionalField).subscribe();
+  }
+
+  add(): void {
+    this.openRegisterModal();
+  }
+
+  openRegisterModal() {
+    this.bsModalRef = this.modalService.show(AddAdditionalFieldComponent, {
+      class: 'modal-lg'
+    });
+    this.bsModalRef.content.closeBtnName = 'Close';
+  }
+
+  openEditModal(item: AdditionalField) {
+    const initialState = {
+      administrator: item
+    };
+    this.bsModalRef = this.modalService.show(EditAdditionalFieldComponent, {
+      class: 'modal-lg',
+      initialState
+    });
+    this.bsModalRef.content.closeBtnName = 'Close';
+  }
 }
