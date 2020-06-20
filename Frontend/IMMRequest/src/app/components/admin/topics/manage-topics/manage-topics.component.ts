e92671c603b8e86b562453346559d897fa525456
Pablo Pereira <invict1@outlook.es>
2020-06-20 02:28:32 -0300

    Se implementó el componente manage-topics relacionado al admin.

--- Frontend/IMMRequest/src/app/components/admin/topics/manage-topics/manage-topics.component.ts
+++ Frontend/IMMRequest/src/app/components/admin/topics/manage-topics/manage-topics.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Topic } from './../../../../models/topic';
+import { AddTopicComponent } from '../add-topic/add-topic.component';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { faUserPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faUserEdit } from '@fortawesome/free-solid-svg-icons';
+import { faUserSlash } from '@fortawesome/free-solid-svg-icons';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { EditTopicComponent } from '../edit-topic/edit-topic.component';
+import { Subscription } from 'rxjs';
+import { TopicService } from '../../../../services/topic.service';
 
 @Component({
   selector: 'app-manage-topics',
@@ -6,10 +15,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-topics.component.css']
 })
 export class ManageTopicsComponent implements OnInit {
+  response: any = { keys: "", body: "" };
+  faUserPlus = faUserPlus;
+  faUserEdit = faUserEdit;
+  faUserRemove = faUserSlash;
+  faSearch = faSearch;
+  topics: Topic[];
+  topic: Topic;
+  subscriptions: Subscription[] = [];
+  bsModalRef: BsModalRef;
+  listFilter: '';
+  constructor(
+    private modalService: BsModalService,
+    private changeDetection: ChangeDetectorRef,
+    private topicService: TopicService
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.modalService.onHide.subscribe(() => {
+      this.topicService
+        .getTopics()
+        .subscribe((topics: Topic[]) => this.topics = topics, messageError => this.response.body = messageError);
+    });
+    this.topicService
+      .getTopics()
+      .subscribe((topics: Topic[]) => this.topics = topics, messageError => this.response.body = messageError);
 
-  ngOnInit(): void {
   }
 
+  edit(item: Topic) {
+    this.openEditModal(item);
+  }
+
+  delete(topic: Topic): void {
+    this.topics = this.topics.filter(h => h !== topic);
+    this.topicService.delete(topic).subscribe();
+  }
+
+  add(): void {
+    this.openRegisterModal();
+  }
+
+  openRegisterModal() {
+    this.bsModalRef = this.modalService.show(AddTopicComponent, {
+      class: 'modal-lg'
+    });
+    this.bsModalRef.content.closeBtnName = 'Close';
+  }
+
+  openEditModal(item: Topic) {
+    const initialState = {
+      administrator: item
+    };
+    this.bsModalRef = this.modalService.show(EditTopicComponent, {
+      class: 'modal-lg',
+      initialState
+    });
+    this.bsModalRef.content.closeBtnName = 'Close';
+  }
 }
