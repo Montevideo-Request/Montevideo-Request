e7f6b5ef98d015028c21bdf6f934cf78f7bfde52
Pablo Pereira <invict1@outlook.es>
2020-06-21 03:34:03 -0300

    Se ajustó el app routing.

--- Frontend/IMMRequest/src/app/app-routing.module.ts
+++ Frontend/IMMRequest/src/app/app-routing.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-import { HomeComponent } from './components/home/home.component';
-import { PanelComponent } from './components/panel/panel.component';
-
-import { NavbarComponent } from './components/navbar/navbar.component';
 import { ManageAdministratorsComponent } from './components/admin/administrators/manage-administrators/manage-administrators.component';
 import { ManageRequestsComponent } from './components/admin/requests/manage-requests/manage-requests.component';
 import { ManageAreasComponent } from './components/admin/areas/manage-areas/manage-areas.component';
 import { ManageTopicsComponent } from './components/admin/topics/manage-topics/manage-topics.component';
 import { DashboardTypesComponent } from './components/admin/types/dashboard-types/dashboard-types.component';
 import { ManageAdditionalFieldsComponent } from './components/admin/additional-fields/manage-additional-fields//manage-additional-fields.component';
-
-import { AdditionalFieldsComponent } from './components/additional-fields/additional-fields.component';
-import { TypesComponent } from './components/types/types.component';
-import { TopicsComponent } from './components/topics/topics.component';
-import { AreasComponent } from './components/areas/areas.component';
 import { RequestComponent } from './components/request/request.component';
-
 import { ReportsComponent } from './components/admin/reports/reports.component';
 import { TypeAComponent } from './components/admin/reports/type-a/type-a.component';
 import { TypeBComponent } from './components/admin/reports/type-b/type-b.component';
@@ -28,54 +17,58 @@ import { LogoutComponent } from './components/logout/logout.component';
 import { AuthenticationErrorComponent } from './components/error/authentication-error/authentication-error.component';
 import { NotFoundComponent } from './components/error/not-found/not-found.component';
 
-import { AdminGuard } from './guards/admin.guard';
 import { AuthGuard } from './guards/auth.guard';
-import { LoginGuard } from './guards/login.guard';
 import { ParserComponent } from './components/parser/parser.component';
 
 export const routes: Routes = [
-    // { path: 'admin', component: HomeComponent },
-    //   {path: 'request/:id', component: RequestComponent, canActivate: [AuthGuard]},
     {
         path: '',
-        component: LoginComponent,
-        canActivate: [LoginGuard]
+        component: LoginComponent
     },
     {
         path: 'admin/administrators',
-        component: ManageAdministratorsComponent
+        component: ManageAdministratorsComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'admin/requests',
-        component: ManageRequestsComponent
+        component: ManageRequestsComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'admin/areas',
-        component: ManageAreasComponent
+        component: ManageAreasComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'admin/types',
-        component: DashboardTypesComponent
+        component: DashboardTypesComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'admin/topics',
-        component: ManageTopicsComponent
+        component: ManageTopicsComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'admin/additionalFields',
-        component: ManageAdditionalFieldsComponent
+        component: ManageAdditionalFieldsComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'admin/reports/typeB',
-        component: TypeBComponent
+        component: TypeBComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'admin/reports/typeA',
-        component: TypeAComponent
+        component: TypeAComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'report',
-        component: ReportsComponent
+        component: ReportsComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'request',
@@ -83,7 +76,8 @@ export const routes: Routes = [
     },
     {
         path: 'parser',
-        component: ParserComponent
+        component: ParserComponent,
+        canActivate: [AuthGuard]
     },
     {
         path: 'logout',
--- Frontend/IMMRequest/src/app/app.module.ts
+++ Frontend/IMMRequest/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { FooterComponent } from './components/footer/footer.component';
@@ -15,15 +14,9 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
-import { WelcomeComponent } from './components/welcome/welcome.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { RequestComponent } from './components/request/request.component';
-import { AreasComponent } from './components/areas/areas.component';
-import { TypesComponent } from './components/types/types.component';
-import { TopicsComponent } from './components/topics/topics.component';
-import { AdditionalFieldsComponent } from './components/additional-fields/additional-fields.component';
-import { PanelComponent } from './components/panel/panel.component';
 import { ManageAreasComponent } from './components/admin/areas/manage-areas/manage-areas.component';
 import { ManageTopicsComponent } from './components/admin/topics/manage-topics/manage-topics.component';
 import { ManageAdditionalFieldsComponent } from './components/admin/additional-fields/manage-additional-fields/manage-additional-fields.component';
@@ -51,8 +44,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { ManageRequestsComponent } from './components/admin/requests/manage-requests/manage-requests.component';
 import { EditRequestComponent } from './components/admin/requests/edit-request/edit-request.component';
 import { EditTypeComponent } from './components/admin/types/edit-type/edit-type.component';
+import { AddTypeComponent } from './components/admin/types/add-type/add-type.component';
 import { DashboardTypesComponent } from './components/admin/types/dashboard-types/dashboard-types.component';
-import { AddTopicComponent } from './components/admin/topics/add-topic/add-topic.component';
 import { EditTopicComponent } from './components/admin/topics/edit-topic/edit-topic.component';
 import { AddAdditionalFieldComponent } from './components/admin/additional-fields/add-additional-field/add-additional-field.component';
 import { EditAdditionalFieldComponent } from './components/admin/additional-fields/edit-additional-field/edit-additional-field.component';
@@ -62,24 +55,18 @@ import { TypeAComponent } from './components/admin/reports/type-a/type-a.compone
 import { TypeBComponent } from './components/admin/reports/type-b/type-b.component';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { DatePipe } from '@angular/common';
+import { CreateTopicComponent } from './components/admin/topics/create-topic/create-topic.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     LoginComponent,
     LogoutComponent,
     FooterComponent,
     ReportsComponent,
     NavbarComponent,
-    WelcomeComponent,
     ManageAdministratorsComponent,
     RequestComponent,
-    AreasComponent,
-    TypesComponent,
-    TopicsComponent,
-    AdditionalFieldsComponent,
-    PanelComponent,
     ManageAreasComponent,
     ManageTopicsComponent,
     ManageAdditionalFieldsComponent,
@@ -99,12 +86,13 @@ import { DatePipe } from '@angular/common';
     RequestsListFilterPipe,
     EditTypeComponent,
     DashboardTypesComponent,
-    AddTopicComponent,
     EditTopicComponent,
     AddAdditionalFieldComponent,
     EditAdditionalFieldComponent,
     TypeAComponent,
-    TypeBComponent
+    TypeBComponent,
+    CreateTopicComponent,
+    AddTypeComponent
   ],
   imports: [
     ModalModule.forRoot(),
