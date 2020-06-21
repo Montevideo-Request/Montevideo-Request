a84ee597d7e60ea9adb4c6263912415427e714cb
Pablo Pereira <invict1@outlook.es>
2020-06-21 03:43:39 -0300

    Se implementó codigo sobre el componente request.

--- Frontend/IMMRequest/src/app/components/request/request.component.ts
+++ Frontend/IMMRequest/src/app/components/request/request.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { faUserPlus, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faUserEdit } from '@fortawesome/free-solid-svg-icons';
+import { faUserSlash } from '@fortawesome/free-solid-svg-icons';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+import { RequestService } from '../../services/request.service';
+import { TypeService } from '../../services/type.service';
+
+import { Request } from './../../models/request';
+import { Type } from './../../models/type';
+import { Guid } from 'guid-typescript';
 
 @Component({
   selector: 'app-request',
@@ -6,10 +19,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./request.component.css']
 })
 export class RequestComponent implements OnInit {
+  types: Type[];
+  requestForm: FormGroup;
+  response: any = { keys: "", body: "" };
+  faUserPlus = faUserPlus;
+  faUserEdit = faUserEdit;
+  faUserRemove = faUserSlash;
+  faSearch = faSearch;
+  faSpinnner = faSpinner;
+  request: Request = new Request(null, null, '', '', '', '', null);
+  submitted = false;
+  error = false;
+  errorMessage = '';
+  bsModalRef: BsModalRef;
+  listFilter: '';
+  constructor(
+    private modalService: BsModalService,
+    private changeDetection: ChangeDetectorRef,
+    private requestService: RequestService,
+    private typeService: TypeService,
+    private formBuilder: FormBuilder
+  ) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.requestForm = this.formBuilder.group({
+      name: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.pattern('^[a-zA-Z ]*$')
+        ])
+      ],
+      phone: ['', Validators.compose([Validators.required])],
+      email: ['', Validators.compose([Validators.required, Validators.email])]
+    });
 
-  ngOnInit(): void {
+    this.typeService
+       .getTypes()
+       .subscribe((types: Type[]) => this.types = types, messageError => this.response.body = messageError);
+
+    // this.administratorService
+    //   .GetAll()
+    //   .subscribe((users: AdministratorBasicInfo[]) => this.administrators = users, messageError => this.response.body = messageError);
+
+    // this.administratorService
+    //   .GetAll()
+    //   .subscribe((users: AdministratorBasicInfo[]) => this.administrators = users, messageError => this.response.body = messageError);
   }
 
+  get r() {
+    return this.requestForm.controls;
+  }
+
+  public submit() {
+    this.submitted = true;
+    if (this.requestForm.invalid) {
+      return;
+    }
+    this.request.Id = Guid.create().toString();
+    console.log(this.request);
+    this.requestService.add(this.request).subscribe(
+      () => {},
+      (error: any) => {
+        this.errorMessage = error;
+        this.error = true;
+      }
+    );
+  }
 }
