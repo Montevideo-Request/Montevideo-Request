4df2c126558b9cdf2e6e370876e98bef7001669c
Pablo Pereira <invict1@outlook.es>
2020-06-21 03:39:34 -0300

    Se ajustó el componente navbar.

--- Frontend/IMMRequest/src/app/components/navbar/navbar.component.ts
+++ Frontend/IMMRequest/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  token: string;
 
-  constructor() { }
+  constructor(private auth: AuthenticationService) { }
 
   ngOnInit(): void {
+    //supongo que deberia controlar cambios al localstorage aca, para saber si mostrar el boton de login o el de logout
   }
-
 }
